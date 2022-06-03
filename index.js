3362351d665629bf9b76aeb61e0c62b4534c4a70
username-pass <classtest303@gmail.com>
2022-06-03 16:12:17 -0700

    1 file modified

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function readTextFile(file, callback) {
   var rawFile = new XMLHttpRequest();
   rawFile.overrideMimeType('application/json');
   rawFile.open('GET', file, true);
-  rawFile.onreadystatechange = function() {
+  rawFile.onreadystatechange = function () {
     if (rawFile.readyState === 4 && rawFile.status == '200') {
       callback(rawFile.responseText);
     }
@@ -16,130 +16,12 @@ function readTextFile(file, callback) {
 }
 
 function jqueryGet(url, callback) {
-  $.get(url, function(responseText) {
+  $.get(url, function (responseText) {
     callback(responseText);
   });
 }
 
-function makePanel() {
-
-
-
-
-  //making the html changer
-  function HTML(input) {
-    panel.innerText = input;
-  }
-
-  //making a panel to append all the other buttons
-
-  var buttonPanel = document.createElement('div');
-  buttonPanel.id = 'bpanel';
-  //buttonPanel.style.float = 'right';
-  //buttonPanel.style.float = 'top';
-  //buttonPanel.style.marginRight = '0px';
-  //buttonPanel.style.marginTop = '0px';
-  buttonPanel.style.right = '0px';
-  buttonPanel.style.top = '0px';
-  buttonPanel.style.position = 'relative';
-  document.getElementsByTagName("body")[0].appendChild(buttonPanel);
-
-
-  //generic button
-  var Button = document.createElement('button');
-  Button.innerHTML = 'Button!';
-  Button.style.backgroundColor = '#333';
-  Button.style.color = '#ccc';
-  Button.onclick = function() {
-    button();
-  };
-  buttonPanel.appendChild(Button);
-
-  //make edit doc button
-  var editDocButton = document.createElement('button');
-  editDocButton.innerHTML = 'edit';
-  editDocButton.style.backgroundColor = '#333';
-  editDocButton.style.color = '#ccc';
-  editDocButton.onclick = function() {
-    editdocbutton();
-  };
-  buttonPanel.appendChild(editDocButton);
-
-  //making scripts button
-  var scriptsButton = document.createElement('button');
-  scriptsButton.innerHTML = 'Scripts';
-  scriptsButton.style.backgroundColor = '#333';
-  scriptsButton.style.color = '#ccc';
-  scriptsButton.onclick = function() {
-    jqueryGet('https://raw.githubusercontent.com/username-pass/ultimate-bml/main/scripts.js',
-      function(txt) {
-        scriptsbutton(txt.split('??'));
-        //alert(txt.split('..'));
-      }
-    );
-  };
-
-  buttonPanel.appendChild(scriptsButton);
-
-  //making document code button
-  var codeButton = document.createElement('button');
-  codeButton.innerHTML = 'docCode';
-  codeButton.style.backgroundColor = '#333';
-  codeButton.style.color = '#ccc';
-  codeButton.onclick = function() {
-    codebutton();
-  };
-  buttonPanel.appendChild(codeButton);
-
-  //xray button
-  var xrayButton = document.createElement('button');
-  xrayButton.innerHTML = 'Xray';
-  xrayButton.style.backgroundColor = '#333';
-  xrayButton.style.color = '#ccc';
-  xrayButton.onclick = function() {
-    xraybutton();
-  };
-  buttonPanel.appendChild(xrayButton);
-  
-    //exit button
-  var exitButton = document.createElement('button');
-  exitButton.innerHTML = 'X';
-  exitButton.style.backgroundColor = '#333';
-  exitButton.style.color = '#ccc';
-  exitButton.onclick = function() {
-    buttonPanel.remove();
-  };
-  buttonPanel.appendChild(exitButton);
-
-  //make info panel inside
-  var panel = document.createElement('div');
-  panel.class = "scroll";
-  panel.style.cssText += 'margin:4px, 4px;padding:4px;width: 256px;height: 256px;overflow-x: hidden;overflow-y: auto;text-align:justify';
-  panel.style.backgroundColor = '#333';
-  panel.style.color = '#ccc';
-  buttonPanel.appendChild(panel);
-  HTML('hello and welcome.... \n \n To the PANEL!!!');
-
-  // .thecode
-  //thestyle.innerText += ".avothecode {border: 1px solid white; margin: 5px; margin-top: 10px; border-radius: 10px; padding: 5px; overflow: scroll; text-align: left; height: 90%;}"
-
-
-
-
-  //make scripts generic function
-  function makeScriptButton(script, appender) {
-    //in name | script format
-    var script = script.split(' | ');
-    var scrpt = document.createElement('button');
-    scrpt.innerHTML = script[0];
-    scrpt.style.backgroundColor = '#333';
-    scrpt.style.color = '#ccc';
-    scrpt.onclick = function() {
-
-    };
-    //alert(script[1]);
-    eval(appender + '.appendChild(scrpt);');
-  }
+function buttons() {
   //button code
 
   function button() {
@@ -181,7 +63,6 @@ function makePanel() {
       HTML('code goes here: \n' + code);
     }
     fetchcode();
-
   }
 
   function xraybutton() {
@@ -193,4 +74,120 @@ function makePanel() {
     document.body.appendChild(script);
   }
 }
+
+function makePanel() {
+  buttons();
+
+  //making the html changer
+  function HTML(input) {
+    panel.innerText = input;
+  }
+
+  //making a panel to append all the other buttons
+
+  var buttonPanel = document.createElement('div');
+  buttonPanel.id = 'bpanel';
+  //buttonPanel.style.float = 'right';
+  //buttonPanel.style.float = 'top';
+  //buttonPanel.style.marginRight = '0px';
+  //buttonPanel.style.marginTop = '0px';
+  buttonPanel.style.right = '0px';
+  buttonPanel.style.top = '0px';
+  buttonPanel.style.position = 'relative';
+  document.getElementsByTagName('body')[0].appendChild(buttonPanel);
+
+  //generic button
+  var Button = document.createElement('button');
+  Button.innerHTML = 'Button!';
+  Button.style.backgroundColor = '#333';
+  Button.style.color = '#ccc';
+  Button.onclick = function () {
+    button();
+  };
+  buttonPanel.appendChild(Button);
+
+  //make edit doc button
+  var editDocButton = document.createElement('button');
+  editDocButton.innerHTML = 'edit';
+  editDocButton.style.backgroundColor = '#333';
+  editDocButton.style.color = '#ccc';
+  editDocButton.onclick = function () {
+    editdocbutton();
+  };
+  buttonPanel.appendChild(editDocButton);
+
+  //making scripts button
+  var scriptsButton = document.createElement('button');
+  scriptsButton.innerHTML = 'Scripts';
+  scriptsButton.style.backgroundColor = '#333';
+  scriptsButton.style.color = '#ccc';
+  scriptsButton.onclick = function () {
+    jqueryGet(
+      'https://raw.githubusercontent.com/username-pass/ultimate-bml/main/scripts.js',
+      function (txt) {
+        scriptsbutton(txt.split('??'));
+        //alert(txt.split('..'));
+      }
+    );
+  };
+
+  buttonPanel.appendChild(scriptsButton);
+
+  //making document code button
+  var codeButton = document.createElement('button');
+  codeButton.innerHTML = 'docCode';
+  codeButton.style.backgroundColor = '#333';
+  codeButton.style.color = '#ccc';
+  codeButton.onclick = function () {
+    codebutton();
+  };
+  buttonPanel.appendChild(codeButton);
+
+  //xray button
+  var xrayButton = document.createElement('button');
+  xrayButton.innerHTML = 'Xray';
+  xrayButton.style.backgroundColor = '#333';
+  xrayButton.style.color = '#ccc';
+  xrayButton.onclick = function () {
+    xraybutton();
+  };
+  buttonPanel.appendChild(xrayButton);
+
+  //exit button
+  var exitButton = document.createElement('button');
+  exitButton.innerHTML = 'X';
+  exitButton.style.backgroundColor = '#333';
+  exitButton.style.color = '#ccc';
+  exitButton.onclick = function () {
+    buttonPanel.remove();
+  };
+  buttonPanel.appendChild(exitButton);
+
+  //make info panel inside
+  var panel = document.createElement('div');
+  panel.class = 'scroll';
+  panel.style.cssText +=
+    'margin:4px, 4px;padding:4px;width: 256px;height: 256px;overflow-x: hidden;overflow-y: auto;text-align:justify';
+  panel.style.backgroundColor = '#333';
+  panel.style.color = '#ccc';
+  buttonPanel.appendChild(panel);
+  HTML('hello and welcome.... \n \n To the PANEL!!!');
+
+  // .thecode
+  //thestyle.innerText += ".avothecode {border: 1px solid white; margin: 5px; margin-top: 10px; border-radius: 10px; padding: 5px; overflow: scroll; text-align: left; height: 90%;}"
+
+  //make scripts generic function
+  function makeScriptButton(script, appender) {
+    //in name | script format
+    var script = script.split(' | ');
+    var scrpt = document.createElement('button');
+    scrpt.innerHTML = script[0];
+    scrpt.style.backgroundColor = '#333';
+    scrpt.style.color = '#ccc';
+    scrpt.onclick = function () {};
+    //alert(script[1]);
+    eval(appender + '.appendChild(scrpt);');
+  }
+}
+
 makePanel();
