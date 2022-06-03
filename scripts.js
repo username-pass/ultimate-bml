a72e6e54181e2dc54904144545e1fff33a0eec42
username-pass <classtest303@gmail.com>
2022-06-03 09:12:36 -0700

    1 file modified

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
-test | prompt('a', '1');
+testp | prompt('a', '1');
 /*urlfinder | alert(window.location);
 3dify | (function(){var js=document.body.appendChild(document.createElement("script"));js.onerror=function(){alert("Sorry, the script could not be loaded.")};js.src="https://rawgit.com/Krazete/bookmarklets/master/tri.js"})();*/
