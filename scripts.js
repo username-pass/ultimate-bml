6c4ba6fa5994fd494d7dd5d1556d92ef8f795586
username-pass <classtest303@gmail.com>
2022-06-03 09:03:51 -0700

    1 file modified

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
-test | alert(1);
+test | alert("a");
 /*urlfinder | alert(window.location);
 3dify | (function(){var js=document.body.appendChild(document.createElement("script"));js.onerror=function(){alert("Sorry, the script could not be loaded.")};js.src="https://rawgit.com/Krazete/bookmarklets/master/tri.js"})();*/
