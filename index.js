d8e3256a7345adae2ccfea620af649ebffee6ca8
Loading <98624927+username-pass@users.noreply.github.com>
2022-06-04 04:41:26 +0000

    slightly improved some stuff, still have scripts bug

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function readTextFile(file, callback) {
   var rawFile = new XMLHttpRequest();
   rawFile.overrideMimeType('application/json');
   rawFile.open('GET', file, true);
-  rawFile.onreadystatechange = function() {
+  rawFile.onreadystatechange = function () {
     if (rawFile.readyState === 4 && rawFile.status == '200') {
       callback(rawFile.responseText);
     }
@@ -16,17 +16,22 @@ function readTextFile(file, callback) {
 }
 
 function jqueryGet(url, callback) {
-  $.get(url, function(responseText) {
+  $.get(url, function (responseText) {
     callback(responseText);
   });
 }
 
+function fetchGet(url,callback){
+	fetch(url)
+  .then(response => response.text())
+  .then(data => {
+  	callback(data);
+  	console.log(data);
+  });
+}
 
 
 function makePanel() {
-
-
-
 
   //making the html changer
   function HTML(input) {
@@ -44,15 +49,15 @@ function makePanel() {
   buttonPanel.style.right = '0px';
   buttonPanel.style.top = '0px';
   buttonPanel.style.position = 'relative';
-  document.getElementsByTagName("body")[0].appendChild(buttonPanel);
-
+  document.getElementsByTagName('body')[0].appendChild(buttonPanel);
 
   //generic button
   var Button = document.createElement('button');
   Button.innerHTML = 'Button!';
   Button.style.backgroundColor = '#333';
   Button.style.color = '#ccc';
-  Button.onclick = function() {
+  
+	Button.onclick = function () {
     button();
   };
   buttonPanel.appendChild(Button);
@@ -62,7 +67,8 @@ function makePanel() {
   editDocButton.innerHTML = 'edit';
   editDocButton.style.backgroundColor = '#333';
   editDocButton.style.color = '#ccc';
-  editDocButton.onclick = function() {
+  
+	editDocButton.onclick = function () {
     editdocbutton();
   };
   buttonPanel.appendChild(editDocButton);
@@ -72,12 +78,15 @@ function makePanel() {
   scriptsButton.innerHTML = 'Scripts';
   scriptsButton.style.backgroundColor = '#333';
   scriptsButton.style.color = '#ccc';
-  scriptsButton.onclick = function() {
-    jqueryGet('https://raw.githubusercontent.com/username-pass/ultimate-bml/main/scripts.js',
-      function(txt) {
+  
+	scriptsButton.onclick = function () {
+    fetchGet(
+      '/scripts.js',
+    function(txt){ 
+			 alert(txt);
         scriptsbutton(txt.split('??'));
         //alert(txt.split('..'));
-      }
+		}
     );
   };
 
@@ -88,7 +97,8 @@ function makePanel() {
   codeButton.innerHTML = 'docCode';
   codeButton.style.backgroundColor = '#333';
   codeButton.style.color = '#ccc';
-  codeButton.onclick = function() {
+  
+	codeButton.onclick = function () {
     codebutton();
   };
   buttonPanel.appendChild(codeButton);
@@ -98,52 +108,105 @@ function makePanel() {
   xrayButton.innerHTML = 'Xray';
   xrayButton.style.backgroundColor = '#333';
   xrayButton.style.color = '#ccc';
-  xrayButton.onclick = function() {
+  xrayButton.onclick = function () {
+		
     xraybutton();
   };
   buttonPanel.appendChild(xrayButton);
-  
-    //exit button
+
+  //exit button
   var exitButton = document.createElement('button');
   exitButton.innerHTML = 'X';
   exitButton.style.backgroundColor = '#333';
   exitButton.style.color = '#ccc';
-  exitButton.onclick = function() {
+  exitButton.onclick = function () {
+		
     buttonPanel.remove();
   };
   buttonPanel.appendChild(exitButton);
 
   //make info panel inside
   var panel = document.createElement('div');
-  panel.class = "scroll";
-  panel.style.cssText += 'margin:4px, 4px;padding:4px;width: 256px;height: 256px;overflow-x: hidden;overflow-y: auto;text-align:justify';
+  panel.class = 'scroll';
+  panel.style.cssText +=
+    'margin:4px, 4px;padding:4px;width: 256px;height: 256px;overflow-x: hidden;overflow-y: auto;text-align:justify';
   panel.style.backgroundColor = '#333';
   panel.style.color = '#ccc';
   buttonPanel.appendChild(panel);
+	
   HTML('hello and welcome.... \n \n To the PANEL!!!');
 
   // .thecode
   //thestyle.innerText += ".avothecode {border: 1px solid white; margin: 5px; margin-top: 10px; border-radius: 10px; padding: 5px; overflow: scroll; text-align: left; height: 90%;}"
 
-
-
-
   //make scripts generic function
-  function makeScriptButton(script, appender) {
+	var scrpt = [];
+  function makeScriptButton(scripty, appender,num) {
     //in name | script format
-    var script = script.split(' | ');
-    var scrpt = document.createElement('button');
-    scrpt.innerHTML = script[0];
-    scrpt.style.backgroundColor = '#333';
-    scrpt.style.color = '#ccc';
-    scrpt.onclick = function() {
+    let scripts = scripty.split(' | ');
+    scrpt[num] = document.createElement('button');
+		scrpt[num].class = 'scriptButton';
+    scrpt[num].style.backgroundColor = '#333';
+    scrpt[num].style.color = '#ccc';		
+    scrpt[num].onclick = console.log(scripts[1]);
+		scrpt[num].innerHTML = scrpt[num].onclick+": "+scripts[0];
 
-    };
-    //alert(script[1]);
-    eval(appender + '.appendChild(scrpt);');
+    eval(appender + '.appendChild(scrpt[num]);');
+  }
+  
+  
+  //button code
+
+  function button() {
+    HTML('generic, why are you here');
   }
 
-}
+  function editdocbutton() {
+    if (document.body.contentEditable !== 'true') {
+      document.body.contentEditable = 'true';
+      document.designMode = 'on';
+      void 0;
+    } else {
+      document.body.contentEditable = 'false';
+      document.designMode = 'off';
+      void 0;
+    }
+    HTML('edit edit edit...');
+  }
 
+  function scriptsbutton(scripts) {
+
+    HTML('scripts: \n');
+    for (i = 0; i < scripts.length; i++) {
+      makeScriptButton(scripts[i], 'panel',i);
+      //if (i % 1 == 0) {
+      panel.innerHTML = panel.innerHTML + '\n';
+      //}
+    }
+    //HTML("scripts go here!");
+  }
+
+  function codebutton() {
+    async function fetchcode() {
+      // Fetch Code
+      var code;
+      var url = await fetch(window.location);
+      var res = await url.text();
+      code = res;
+      HTML('code goes here: \n' + code);
+    }
+    fetchcode();
+  }
+
+  function xraybutton() {
+    var script = document.createElement('script');
+    script.src = 'https://x-ray-goggles.mouse.org/webxray.js';
+    script.className = 'webxray';
+    script.setAttribute('data-lang', 'en-US');
+    script.setAttribute('data-baseuri', 'https://x-ray-goggles.mouse.org');
+    document.body.appendChild(script);
+  }
+  
+}
 
 makePanel();
