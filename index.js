4e3349ba248d0a34f4ef3dc57ecb0daab5e3acf3
Loading <98624927+username-pass@users.noreply.github.com>
2022-06-05 03:02:22 +0000

    still have scripts bug, added console functionality...

--- index.js
+++ index.js
@@ -15,6 +15,35 @@ function readTextFile(file, callback) {
   rawFile.send(null);
 }
 
+function getConsole(){
+if (console.everything === undefined)
+{
+    console.everything = [];
+
+    console.defaultLog = console.log.bind(console);
+    console.log = function(){
+        console.everything.push({"type":"log", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+        console.defaultLog.apply(console, arguments);
+    }
+    console.defaultError = console.error.bind(console);
+    console.error = function(){
+        console.everything.push({"type":"error", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+        console.defaultError.apply(console, arguments);
+    }
+    console.defaultWarn = console.warn.bind(console);
+    console.warn = function(){
+        console.everything.push({"type":"warn", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+        console.defaultWarn.apply(console, arguments);
+    }
+    console.defaultDebug = console.debug.bind(console);
+    console.debug = function(){
+        console.everything.push({"type":"debug", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+        console.defaultDebug.apply(console, arguments);
+    }
+}
+}
+getConsole();
+
 function jqueryGet(url, callback) {
   $.get(url, function (responseText) {
     callback(responseText);
@@ -60,11 +89,11 @@ function makePanel() {
 	Button.onclick = function () {
     button();
   };
-  buttonPanel.appendChild(Button);
+//  buttonPanel.appendChild(Button);
 
   //make edit doc button
   var editDocButton = document.createElement('button');
-  editDocButton.innerHTML = 'edit';
+  editDocButton.innerHTML = '📝';
   editDocButton.style.backgroundColor = '#333';
   editDocButton.style.color = '#ccc';
   
@@ -76,6 +105,7 @@ function makePanel() {
   //making scripts button
   var scriptsButton = document.createElement('button');
   scriptsButton.innerHTML = 'Scripts';
+	
   scriptsButton.style.backgroundColor = '#333';
   scriptsButton.style.color = '#ccc';
   
@@ -113,6 +143,16 @@ function makePanel() {
     xraybutton();
   };
   buttonPanel.appendChild(xrayButton);
+	  //generic button
+  var consoleButton = document.createElement('button');
+  consoleButton.innerHTML = 'Console';
+  consoleButton.style.backgroundColor = '#333';
+  consoleButton.style.color = '#ccc';
+  
+	consoleButton.onclick = function () {
+    consolebutton();
+  };
+  buttonPanel.appendChild(consoleButton);
 
   //exit button
   var exitButton = document.createElement('button');
@@ -172,11 +212,13 @@ function makePanel() {
       void 0;
     }
     HTML('edit edit edit...');
+		console.log("edit button");
   }
 
   function scriptsbutton(scripts) {
 
     HTML('scripts: \n');
+		console.log("scripts button");
     for (i = 0; i < scripts.length; i++) {
       makeScriptButton(scripts[i], 'panel',i);
       //if (i % 1 == 0) {
@@ -196,6 +238,24 @@ function makePanel() {
       HTML('code goes here: \n' + code);
     }
     fetchcode();
+		console.log("code button");
+  }
+	
+
+	function consolebutton() {
+
+			console.log("console");
+			let cons = "";
+			for(i=0;i<console.everything.length-1;i++){
+				let coev = console.everything[i];
+				let val = String(coev.value);
+				val = val.trim();
+				cons = cons+""+coev.type+": "+val+"\n";
+			}
+		
+      HTML('console: \n' + cons);
+    
+    
   }
 
   function xraybutton() {
